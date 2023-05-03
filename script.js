29a9e520b475baeb4218a41ecf2e7cb5987cf2c4
Hiwad Stanekzai <Hiwad_stanekzai@yahoo.com>
2023-05-02 22:19:01 -0700

    Base library template set up

---
+++ script.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+function Book() {
+    //constructor
+}
+
+function addBookToLibrary() {
+
+}
+
+function displayBook() {
+    for (let i = 0; i < myLibrary.length; ++i) {
+        //display each book on the page
+    }
+}
